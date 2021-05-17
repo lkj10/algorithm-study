53bc58c55bb9de132c691e8801c16fba1f985234
isun-dev <florachoi1124@naver.com>
2021-05-18 00:40:19 +0900

    프로그래머스 - 완주하지 못한 선수 - 두번째 풀이(통과)

--- insun/anUnfinishedRunner.py
+++ insun/anUnfinishedRunner.py
@@ -1,3 +1,6 @@
+import collections
+
+
 # 첫번째 풀이 - 정확성은 모두 통과, 효율성은 2개 실패
 def solution(participant, completion):
     participant.sort()
@@ -8,3 +11,10 @@ def solution(participant, completion):
             completion.remove(answer)
         else:
             return answer
+
+
+# 두번째 풀이 - collection.Counter()를 사용
+# Counter()는 리스트 원소들의 개수를 알고 싶을 때 사용 가능
+def solution2(participant, completion):
+    answer2 = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer2.keys())[0]
