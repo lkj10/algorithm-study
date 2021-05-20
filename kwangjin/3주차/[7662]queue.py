3ebc99132ac7bf8fa91f5f61073aeba044454e7e
lkj10 <57061504+lkj10@users.noreply.github.com>
2021-05-21 07:01:22 +0900

    [7662]queue.py solving...
    양측에서 중복된 값 제거를 못하겠음.
    채점결과 시간초과로 떠서 그 부분 처리 해야함.

--- kwangjin/3주차/[7662]queue.py
+++ kwangjin/3주차/[7662]queue.py
@@ -7,23 +7,34 @@ for _ in range(T):
     k = int(input())
     MAX_List = list()
     MIN_List = list()
+    List = list()
     for i in range(k):
         M, N = map(str, input().split())
-        print(MAX_List, MIN_List)
         if M == 'I':
             heapq.heappush(MAX_List, -int(N))
             heapq.heappush(MIN_List, int(N))
         elif M == 'D':
-            if len(MAX_List + MIN_List) == 0 :
+            if len(MAX_List) == 0 or len(MIN_List) == 0 :
                 continue
             if int(N) == 1:
-                heapq.heappop(MAX_List)*-1
-                
+                List.append(heapq.heappop(MAX_List)*-1)
             elif int(N) == -1:
-                heapq.heappop(MIN_List)
-    else :
-        if len(MAX_List + MIN_List) > 0 :
-            print(heapq.heappop(MAX_List)*-1, heapq.heappop(MIN_List))
+                List.append(heapq.heappop(MIN_List))
+
+            if len(MAX_List) == 0 or len(MIN_List) == 0 :
+                MAX_List.clear()
+                MIN_List.clear()
+
+    for i in List:
+        if -i in MAX_List:
+            MAX_List.pop(MAX_List.index(-i))
+        if i in MIN_List:
+            MIN_List.pop(MIN_List.index(i))
+    
+    if len(MAX_List + MIN_List) > 0 :
+        print(heapq.heappop(MAX_List)*-1, heapq.heappop(MIN_List))
+    else:
+        print("EMPTY")
 
 
 
