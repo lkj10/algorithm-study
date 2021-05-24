14f660c98772a1d973a842efa3071b13224807f4
lkj10 <57061504+lkj10@users.noreply.github.com>
2021-05-24 17:30:21 +0900

    [2775]will_be_chairman.py solved!

--- kwangjin/self-study/2021.05.20/[2775]will_be_chairman.py
+++ kwangjin/self-study/2021.05.20/[2775]will_be_chairman.py
@@ -4,5 +4,14 @@ sys.stdin = open("input.txt", "r")
 T = int(input())
 
 for _ in range(T):
-    k = int(input())
-    n = int(input())
+    K = int(input())
+    N = int(input())
+    List = [i for i in range(1, N+2)]
+
+    for i in range(K):
+        for j in range(1, N+1):
+            List[j] += List[j-1]
+    print(List[N-1])
+
+
+
