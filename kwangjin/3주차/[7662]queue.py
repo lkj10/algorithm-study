34845451d367eae2e2f3f9c1b1a33b3ffbde734b
lkj10 <57061504+lkj10@users.noreply.github.com>
2021-05-24 12:23:15 +0900

    [7662]queue.py soving...
    dict로 동기화 시도했으나 1%에서 틀림

--- kwangjin/3주차/[7662]queue.py
+++ kwangjin/3주차/[7662]queue.py
@@ -7,32 +7,33 @@ for _ in range(T):
     k = int(input())
     MAX_List = list()
     MIN_List = list()
-    List = list()
+    dic = {}
+    result = list()
+    temp = 0
     for i in range(k):
         M, N = map(str, input().split())
         if M == 'I':
             heapq.heappush(MAX_List, -int(N))
             heapq.heappush(MIN_List, int(N))
+            dic[int(N)] = 1
         elif M == 'D':
             if len(MAX_List) == 0 or len(MIN_List) == 0 :
                 continue
             if int(N) == 1:
-                List.append(heapq.heappop(MAX_List)*-1)
+                temp = heapq.heappop(MAX_List)*-1
+                dic[temp] = 0
             elif int(N) == -1:
-                List.append(heapq.heappop(MIN_List))
-
-            if len(MAX_List) == 0 or len(MIN_List) == 0 :
-                MAX_List.clear()
-                MIN_List.clear()
-
-    for i in List:
-        if -i in MAX_List:
-            MAX_List.pop(MAX_List.index(-i))
-        if i in MIN_List:
-            MIN_List.pop(MIN_List.index(i))
-    
-    if len(MAX_List + MIN_List) > 0 :
-        print(heapq.heappop(MAX_List)*-1, heapq.heappop(MIN_List))
+                temp = heapq.heappop(MIN_List)
+                dic[temp] = 0
+            # if len(MAX_List) == 0 or len(MIN_List) == 0 :
+            #     MAX_List.clear()
+            #     MIN_List.clear()
+    for idx, val in dic.items():
+        if val == 1:
+            result.append(idx)
+            
+    if len(result) > 1 :
+        print(max(result), min(result))
     else:
         print("EMPTY")
 
