2321b0da48c2b6506438e48659b5f837d3046a85
jangwon <yoonjangwon94@gmail.com>
2021-06-21 10:04:02 +0900

    feat: boj 205 이분탐색 solved

---
+++ jangwon/6주차/6-1/수강과목.py
@@ -0,0 +1,18 @@
+# # DP g
+
+# # N, K (최대 공부시간, 과목 수)
+
+# import sys
+# import heapq
+# sys.stdin = open("input.txt", "r")
+
+# N, K = map(int, input().split())
+
+# hq = []
+# importance, time = [] , []
+
+# for _ in range(K):
+#   I, T = map(int, input().split())
+
+
+#   # 힙으로 풀이 
