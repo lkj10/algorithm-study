334c1197ef451a384f7f257c8f8fcd112e00839e
jangwon <yoonjangwon94@gmail.com>
2021-06-23 20:10:19 +0900

    feat: [self] sixshop, floyd whashal boj1389 solved

---
+++ jangwon/7주차/sixshop.py
@@ -0,0 +1,26 @@
+INF=int(1e9)
+def solution(n,relation):
+    answer=[0]*(n+1)
+    graph=[[INF]*(n+1) for _ in range(n+1)]
+    for i in range(1,n+1):
+        graph[i][i]=0
+    for a,b in relation:
+        graph[a][b]=1
+        graph[b][a]=1
+
+    for k in range(1,n+1):
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                graph[i][j]=min(graph[i][j],graph[i][k]+graph[k][j])
+
+    for row in range(1,n+1):
+        count=0
+        for x in range(1,n+1):
+            if graph[row][x]==1 or graph[row][x]==2:
+                count+=1
+        answer[row]=count
+    return answer[1:]
+
+
+print(solution(5,[[1,2],[4,2],[3,1],[4,5]]))
+print(solution(7,[[1,2],[4,2],[3,1],[4,5],[6,7]]))
