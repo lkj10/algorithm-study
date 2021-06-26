70e3c1ff7b07f9c88f43822ce67d981ecba251d5
lkj10 <lkj10@users.noreply.github.com>
2021-06-26 17:25:35 +0900

    feat:prog new_id_reco.py resolved!
    정규표현식으로 다시 문제를 품.

--- kwangjin/self-study/2021.06.25/new_id_reco.py
+++ kwangjin/self-study/2021.06.25/new_id_reco.py
@@ -1,19 +1,22 @@
+import re
+
+
 def solution(new_id):
     answer = ''
     List = list(new_id)
     temp = list()
     temp2 = list()
     cnt = 0
-    #step 1
+    # step 1
     for i in range(len(List)):
         if List[i].isalpha():
             List[i] = List[i].lower()
-            
-    #step 2
+
+    # step 2
     for i in range(len(List)):
         if List[i].isalpha() or List[i].isdecimal() or List[i] == '-' or List[i] == '_' or List[i] == '.':
             temp.append(List[i])
-    #step 3
+    # step 3
     for i in temp:
         temp2.append(i)
         if i == '.':
@@ -23,27 +26,40 @@ def solution(new_id):
                 cnt = 1
         else:
             cnt = 0
-            
-    #step 4
-    if temp2 and temp2[0] == '.' :
+
+    # step 4
+    if temp2 and temp2[0] == '.':
         temp2.pop(0)
     if temp2 and temp2[-1] == '.':
         temp2.pop()
-        
-    #step 5
+
+    # step 5
     if temp2 == []:
         temp2.append('a')
-    
-    #step 6
+
+    # step 6
     if len(temp2) >= 16:
         temp2 = temp2[:15]
         if temp2[-1] == '.':
             temp2.pop()
-    
-    #step 7
+
+    # step 7
     if len(temp2) <= 2:
         temp_str = temp2[-1]
         while len(temp2) < 3:
             temp2.append(temp_str)
 
     return ''.join(temp2)
+
+
+def solution(new_id):
+    #new_id = re.sub('[A-Z]+', lambda m : m.group().lower(), new_id)
+    new_id = new_id.lower()
+    new_id = re.sub('[^a-z0-9\-_\.]', '', new_id)
+    new_id = re.sub('\.+', '.', new_id)
+    new_id = re.sub('^\.|\.$', '', new_id)
+    new_id = ('a' if len(new_id) == 0 else new_id[:15])
+    new_id = re.sub('\.$', '', new_id)
+    new_id = new_id + new_id[-1] * \
+        (3 - len(new_id)) if len(new_id) < 3 else new_id
+    return new_id
