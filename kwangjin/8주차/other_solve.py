2c76c5a20305ffa45db4d479226be1c2797dafc4
lkj10 <lkj10@users.noreply.github.com>
2021-06-27 14:49:55 +0900

    feat:8주차 prog other_solve.py solved!
    가독성 쉽게 commit 수정

--- kwangjin/8주차/other_solve.py
+++ kwangjin/8주차/other_solve.py
@@ -1,9 +1,16 @@
-#import re
+import re
+
+
 def solution(s):
-    # while re.search('(([a-zA-Z0-9])\\2{1})',s):
-    #     s = re.sub('(([a-zA-Z0-9])\\2{1})', '', s)
-    # return 0 if s else 1
     stack = list()
     for i in s:
         stack.pop() if stack and stack[-1] == i else stack.append(i)
     return 0 if stack else 1
+
+# 정규표현식으로 품. 그러나 시간초과 이슈
+
+
+def solution(s):
+    while re.search('(([a-zA-Z0-9])\\2{1})', s):
+        s = re.sub('(([a-zA-Z0-9])\\2{1})', '', s)
+    return 0 if s else 1
