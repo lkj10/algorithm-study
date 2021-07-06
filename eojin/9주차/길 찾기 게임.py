54b5d659ef4f0fe566a58e510dd0f2a688a9d9df
EoJin-Kim <62640679+EoJin-Kim@users.noreply.github.com>
2021-07-06 23:44:29 +0900

    fix:[8주차] prog 트리 길찾기게임 solved

--- eojin/9주차/길 찾기 게임.py
+++ eojin/9주차/길 찾기 게임.py
@@ -3,8 +3,7 @@ import sys
 sys.setrecursionlimit(10 ** 6)
 
 
-
-def preorder(arrY,  answer):
+def tree_order(arrY, pre_answer,post_answer):
     node = arrY[0]
 
     arrY1 = []
@@ -15,48 +14,25 @@ def preorder(arrY,  answer):
             arrY1.append(arrY[i])
         else:
             arrY2.append(arrY[i])
-
-    answer.append(node[2])
+    pre_answer.append(node[2])
     if len(arrY1) > 0:
-        preorder(arrY1, answer)
+        tree_order(arrY1,  pre_answer,post_answer)
     if len(arrY2) > 0:
-        preorder(arrY2, answer)
+        tree_order(arrY2,  pre_answer,post_answer)
+    post_answer.append(node[2])
     return
-
-def postorder(arrY, answer):
-    node = arrY[0]
-
-    arrY1 = []
-    arrY2 = []
-
-    for i in range(1, len(arrY)):
-        if node[0] > arrY[i][0]:
-            arrY1.append(arrY[i])
-        else:
-            arrY2.append(arrY[i])
-
-    if len(arrY1) > 0:
-        postorder(arrY1,  answer)
-    if len(arrY2) > 0:
-        postorder(arrY2,  answer)
-    answer.append(node[2])
-    return
-
 
 def solution(nodeinfo):
-    preanswer = []
-    postanswer = []
+    pre_answer = []
+    post_answer = []
 
     for i in range(len(nodeinfo)):
         nodeinfo[i].append(i + 1)
 
     arrY = sorted(nodeinfo, key=lambda x: (-x[1], x[0]))
 
-
-    preorder(arrY,  preanswer)
-    postorder(arrY, postanswer)
-
-    return [preanswer, postanswer]
+    tree_order(arrY,pre_answer,post_answer)
+    return [pre_answer, post_answer]
 
 
 print(solution([[5,3],[11,5],[13,3],[3,5],[6,1],[1,3],[8,6],[7,2],[2,2]]))
