08b66c2231424b81704f2c253ceccba42472d1a3
lkj10 <57061504+lkj10@users.noreply.github.com>
2021-09-23 17:01:43 +0900

    feat:19주차 boj [12100]2048 Easy.py solved! dfs

--- kwangjin/19주차/[12100]2048 Easy.py
+++ kwangjin/19주차/[12100]2048 Easy.py
@@ -1,14 +1,15 @@
 import sys
 from collections import deque
+import copy
 sys.stdin = open("input.txt", "r")
 
 N = int(input())
-arr = []
+List = []
 for _ in range(N):
-    arr.append(list(map(int, input().split())))
+    List.append(list(map(int, input().split())))
+ans = 0
 
-
-def move_and_merge(List, dir):
+def move_and_merge(dir):
     if dir == 0: # left
         for y in range(N):
             idx = deque()
@@ -25,6 +26,16 @@ def move_and_merge(List, dir):
                 if List[y][x] == List[y][x+1]:
                     List[y][x] = List[y][x]*2
                     List[y][x+1] = 0
+    
+        for y in range(N):
+            idx = deque()
+            for x in range(N):
+                if List[y][x] == 0:
+                    idx.append(x)
+                elif idx:
+                    List[y][idx.popleft()] = List[y][x]
+                    List[y][x] = 0
+                    idx.append(x)
 
     if dir == 1: # up
         for x in range(N):
@@ -43,6 +54,16 @@ def move_and_merge(List, dir):
                     List[y][x] = List[y][x]*2
                     List[y+1][x] = 0
 
+        for x in range(N):
+            idx = deque()
+            for y in range(N):
+                if List[y][x] == 0:
+                    idx.append(y)
+                elif idx:
+                    List[idx.popleft()][x] = List[y][x]
+                    List[y][x] = 0
+                    idx.append(y)
+
     if dir == 2: # right
         for y in range(N):
             idx = deque()
@@ -60,6 +81,17 @@ def move_and_merge(List, dir):
                     List[y][x] = List[y][x]*2
                     List[y][x-1] = 0
 
+        for y in range(N):
+            idx = deque()
+            for x in range(N-1, -1, -1):
+                if List[y][x] == 0:
+                    idx.append(x)
+                elif idx:
+                    List[y][idx.popleft()] = List[y][x]
+                    List[y][x] = 0
+                    idx.append(x)
+
+
     if dir == 3: # down
         for x in range(N):
             idx = deque()
@@ -76,16 +108,29 @@ def move_and_merge(List, dir):
                 if List[y][x] == List[y-1][x]:
                     List[y][x] = List[y][x]*2
                     List[y-1][x] = 0
+                    
+        for x in range(N):
+            idx = deque()
+            for y in range(N-1, -1, -1):
+                if List[y][x] == 0:
+                    idx.append(y)
+                elif idx:
+                    List[idx.popleft()][x] = List[y][x]
+                    List[y][x] = 0
+                    idx.append(y)
 
-
-def dfs(List, level):
+def dfs(level):
+    global ans, List
     if level == 5:
-        print(List)
+        for i in range(N):
+            ans = max(ans, max(List[i]))
         return
+    tmp = copy.deepcopy(List)
+    for i in range(4):
+        move_and_merge(i)
+        dfs(level+1)
+        List = copy.deepcopy(tmp)
 
-    for i in range(2):
-        move_and_merge(List, i)
-        dfs(List, level+1)
 
-
-dfs(arr, 0)
+dfs(0)
+print(ans)
