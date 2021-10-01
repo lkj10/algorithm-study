b41daa1f81e3d14d5d1db42c1829425cb3289da0
lkj10 <57061504+lkj10@users.noreply.github.com>
2021-10-01 17:19:47 +0900

    feat:solving...
    문제 만들어놓고 아직 안품

---
+++ kwangjin/20주차/[6064]kaing.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+T = int(input())
+
+for _ in range(T):
+    M, N, x, y = map(int, input().split())
+
+    1 1
+    2 2
+    3 3
+    4 4
+    5 5
+    6 6
+    7 7
+    8 8
+    9 9
+    1 10
+    2 11
+    3 1
+    4 2
+    5 3
+    6 4
+    7 5
+    8 6
+    9 7
+    1 8
+    2 9
+    3 10
+    4 11
+    
