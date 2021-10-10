3b019be78100a1541fac69df2061e618f03ced65
lkj10 <57061504+lkj10@users.noreply.github.com>
2021-10-10 22:52:45 +0900

    feat:20주차 boj [7569]tomato.py solved! bfs
    드뎌 풀었다

--- kwangjin/20주차/[7569]tomato.py
+++ kwangjin/20주차/[7569]tomato.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 sys.stdin = open("input.txt", "r")
 
 List = []
@@ -9,48 +10,41 @@ for _ in range(N*H):
 
 dx = [-1, 0, 1, 0, 0, 0]
 dy = [0, 1, 0, -1, -N, N]
-day = 0
 
-def foo():
-    global day
-    while(1):
-        flag = 0
-        res = [[0]*M for _ in range(N*H)]
+dq = deque()
 
-        for y in range(N*H):
-            for x in range(M):
-                res[y][x] = List[y][x]
+def bfs():
+    while(dq):
+        z, y, x = dq.popleft()
+        for i in range(4):
+            xx = x + dx[i]
+            yy = y + dy[i]
+            if 0 <= xx < M and z*N <= yy < z*N + N and List[yy][xx] == 0:
+                List[yy][xx] = List[y][x] + 1
+                dq.append((z, yy, xx))
+        for i in range(4, 6):
+            xx = x + dx[i]
+            yy = y + dy[i]
+            if 0 <= xx < M and 0 <= yy < H*N and List[yy][xx] == 0:
+                List[yy][xx] = List[y][x] + 1
+                dq.append((z+dy[i]/N, yy, xx))
+    
 
-        for h in range(H):
-            for y in range(h*N, h*N + N):
-                for x in range(M):
-                    if List[y][x] == 1:
-                        for i in range(4):
-                            yy = y + dy[i]
-                            xx = x + dx[i]
-                            if  h*N <= yy < h*N + N and 0 <= xx < M and List[yy][xx] == 0:
-                                res[yy][xx] = 1
-                                flag = 1
+def check():
+    MAX = 0
+    for y in range(N*H):
+        for x in range(M):
+            if List[y][x] == 0:
+                print(-1)
+                return
+            MAX = max(List[y][x], MAX)
+    print(MAX-1)
 
-                        for i in range(4, 5):
-                            yy = y + dy[i]
-                            xx = x + dx[i]
-                            if  0 <= yy < N*H and List[yy][xx] == 0:
-                                res[yy][xx] = 1
-                                flag = 1
-        if flag == 0:
-            for y in range(N*H):
-                for x in range(M):
-                    if List[y][x] == 0:
-                        print(-1)
-                        return
-            print(day)
-            return
+for z in range(H):
+    for y in range(z*N, z*N + N):
+        for x in range(M):
+            if List[y][x] > 0:
+                dq.append((z, y, x))
 
-        for y in range(N*H):
-            for x in range(M):
-                List[y][x] = res[y][x]
-
-        day += 1
-
-foo()
+bfs()
+check()
