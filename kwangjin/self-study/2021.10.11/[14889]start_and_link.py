9d68e2c20e6fb572f92713462ab9cdc0051c1f0f
lkj10 <57061504+lkj10@users.noreply.github.com>
2021-10-13 01:42:23 +0900

    feat[14889]start_and_link.py solved! dfs
    변수 이름에 주의하자

--- kwangjin/self-study/2021.10.11/[14889]start_and_link.py
+++ kwangjin/self-study/2021.10.11/[14889]start_and_link.py
@@ -1,3 +1,28 @@
 import sys
-sys.stdin = open("input.txt", "r")
+sys.setrecursionlimit(100001)
+T = int(sys.stdin.readline())
+List = [list(map(int, sys.stdin.readline().split())) for _ in range(T)]
+visit = [0 for _ in range(T)]
+res = 2100000000
 
+def dfs(idx, level):
+    global res
+    if level == T//2:
+        SUM_start, SUM_link = 0, 0
+        for y in range(T):
+            for x in range(y+1, T):
+                if visit[y] and visit[x] :
+                    SUM_start += List[y][x] + List[x][y]
+                elif not visit[y] and not visit[x]:
+                    SUM_link += List[y][x] + List[x][y]
+        res = min(res, abs(SUM_start - SUM_link))
+
+    for i in range(idx, T-1):
+        if visit[i]:
+            continue
+        visit[i] = 1
+        dfs(i+1, level+1)
+        visit[i] = 0
+
+dfs(0, 0)
+print(res)
