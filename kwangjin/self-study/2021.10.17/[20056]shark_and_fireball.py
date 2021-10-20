45d83e81ef1f59428aaab8df6aa0da6d44806452
lkj10 <57061504+lkj10@users.noreply.github.com>
2021-10-20 14:33:15 +0900

    feat:[20056]shark_and_fireball.py solved!

--- kwangjin/self-study/2021.10.17/[20056]shark_and_fireball.py
+++ kwangjin/self-study/2021.10.17/[20056]shark_and_fireball.py
@@ -2,67 +2,71 @@ import sys
 import copy
 sys.stdin = open("input.txt", "r")
 
-N, M, K = map(int, input().split())
-Map = [[0]*N for _ in range(N)]  # 질량
-visit = [[0]*N for _ in range(N)]  # 몇 개 있는지
-vector = [[0]*N for _ in range(N)]  # 속력
-dir = [[[] for _ in range(N)] for _ in range(N)]
-
 dy = [-1, -1, 0, 1, 1, 1, 0, -1]
 dx = [0, 1, 1, 1, 0, -1, -1, -1]
-
 
 def move():
     for y in range(N):
         for x in range(N):
-            if visit[y][x] != 0:
-                yy, xx = y, x
+            if visit[y][x] > 0:
                 for i in range(len(dir[y][x])):
-                    yy = (yy + dy[dir[y][x][i]]*vector[y][x]) % N
-                    xx = (xx + dx[dir[y][x][i]]*vector[y][x]) % N
+                    yy = (y + dy[dir[y][x][i]] * vec[y][x]) % N
+                    xx = (x + dx[dir[y][x][i]] * vec[y][x]) % N
                     temp_Map[yy][xx] += Map[y][x]
-                    temp_visit[yy][xx] += 1
-                    temp_vector[yy][xx] += vector[y][x]
                     temp_dir[yy][xx].append(dir[y][x][i])
+                    temp_visit[yy][xx] += 1
+                    temp_vec[yy][xx] += vec[y][x]
 
-
-def merge_and_divide():
     for y in range(N):
         for x in range(N):
-            if visit[y][x] > 1:
-                yy, xx = y, x
-                for i in range(len(dir[y][x])):
-                    yy = (yy + dy[dir[y][x][i]]*vector[y][x]) % N
-                    xx = (xx + dx[dir[y][x][i]]*vector[y][x]) % N
-                    temp_Map[yy][xx] += Map[y][x]
-                    temp_visit[yy][xx] += 1
-                    temp_vector[yy][xx] += vector[y][x]
-                    temp_dir[yy][xx].append(dir[y][x][i])
+            if temp_visit[y][x] > 1:
+                temp_Map[y][x] = temp_Map[y][x]//5
+                if temp_Map[y][x] == 0:
+                    temp_visit[y][x] = 0
+                    continue
+                temp_vec[y][x] = temp_vec[y][x] // temp_visit[y][x]
+                odd, even = 0, 0
+                for i in range(len(temp_dir[y][x])):
+                    if temp_dir[y][x][i] % 2 == 0:
+                        even = 1
+                    else: 
+                        odd = 1
+                if odd + even == 1 :
+                    temp_dir[y][x] = [0, 2, 4, 6]
+                else:
+                    temp_dir[y][x] = [1, 3, 5, 7]
 
+N, M, K = map(int, input().split())
+
+Map = [[0]*N for _ in range(N)]
+visit = [[0]*N for _ in range(N)]
+dir = [[[] for _ in range(N)] for _ in range(N)]
+vec = [[0]*N for _ in range(N)]
 
 for _ in range(M):
-    # r = 행, c = 열, m = 질량, s = 속도, d = 방향
     r, c, m, s, d = map(int, input().split())
     Map[r-1][c-1] = m
-    visit[r-1][c-1] = 1
-    vector[r-1][c-1] = s
+    visit[r-1][c-1] += 1
     dir[r-1][c-1].append(d)
-
-for i in Map:
-    print(i)
-print()
+    vec[r-1][c-1] = s
 
 for _ in range(K):
-    temp_Map = [[0]*N for _ in range(N)]  # 질량
-    temp_visit = [[0]*N for _ in range(N)]  # 몇 개 있는지
-    temp_vector = [[0]*N for _ in range(N)]  # 속력
+    temp_Map = [[0]*N for _ in range(N)]
+    temp_visit = [[0]*N for _ in range(N)]
+    temp_vec = [[0]*N for _ in range(N)]
     temp_dir = [[[] for _ in range(N)] for _ in range(N)]
+    
     move()
-    merge_and_divide()
     Map = copy.deepcopy(temp_Map)
     visit = copy.deepcopy(temp_visit)
-    vector = copy.deepcopy(temp_vector)
     dir = copy.deepcopy(temp_dir)
-    for i in Map:
-        print(i)
-    print()
+    vec = copy.deepcopy(temp_vec)
+
+ans = 0
+for y in range(N):
+    for x in range(N):
+        if visit[y][x] > 1:
+            ans += Map[y][x]*4
+        elif visit[y][x] == 1:
+            ans += Map[y][x]
+print(ans)
