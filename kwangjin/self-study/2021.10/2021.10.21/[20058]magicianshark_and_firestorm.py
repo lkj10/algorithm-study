d1775fe713802e71186ce13c146aa75a35bbcc25
lkj10 <lee_10_@naver.com>
2021-10-23 21:51:07 +0900

    feat:study re
    틀린 부분 확실히 찾을 것.

--- kwangjin/self-study/2021.10/2021.10.21/[20058]magicianshark_and_firestorm.py
+++ kwangjin/self-study/2021.10/2021.10.21/[20058]magicianshark_and_firestorm.py
@@ -10,6 +10,7 @@ list_Q = list(map(int, input().split()))
 dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 
+
 def foo(L):
     for y in range(0, pow(2, N), L):
         for x in range(0, pow(2, N), L):
@@ -34,26 +35,25 @@ def foo(L):
                 temp_Map[y][x] -= 1
                 if temp_Map[y][x] < 0:
                     temp_Map[y][x] = 0
-    
+
     return temp_Map
 
 
 def bfs(y, x):
     global Max
     visit[y][x] = 1
-    dq = deque([[y,x]])
+    dq = deque([[y, x]])
     ice_cnt = 1
     while dq:
         yy, xx = dq.popleft()
         for i in range(4):
-                yyy = yy + dy[i]
-                xxx = xx + dx[i]
-                if 0 <= yyy < (pow(2, N)) and 0 <= xxx < (pow(2, N)) and visit[yyy][xxx] == 0 and Map[yyy][xxx] > 0:
-                    visit[yyy][xxx] = 1
-                    dq.append([yyy, xxx])
-                    ice_cnt += 1
+            yyy = yy + dy[i]
+            xxx = xx + dx[i]
+            if 0 <= yyy < (pow(2, N)) and 0 <= xxx < (pow(2, N)) and visit[yyy][xxx] == 0 and Map[yyy][xxx] > 0:
+                visit[yyy][xxx] = 1
+                dq.append([yyy, xxx])
+                ice_cnt += 1
     Max = max(Max, ice_cnt)
-
 
 
 for i in range(Q):
--- kwangjin/self-study/2021.10/2021.10.23/[20058](re)magicianshark_and_firestorm.py
+++ kwangjin/self-study/2021.10/2021.10.23/[20058](re)magicianshark_and_firestorm.py
@@ -1,2 +1,79 @@
 import sys
+import copy
+from collections import deque
 sys.stdin = open("input.txt", "r")
+
+N, Q = map(int, input().split())
+
+List = [list(map(int, input().split())) for _ in range(pow(2, N))]
+
+cmd_List = list(map(int, input().split()))
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+
+def firestorm(size):
+    for y in range(0, pow(2, N), size):
+        for x in range(0, pow(2, N), size):
+            size_temp_List = [[0]*size for _ in range(size)]
+            for yy in range(y, y + size):
+                for xx in range(x, x + size):
+                    size_temp_List[xx-x][size-yy+y-1] = List[yy][xx]
+
+            for yy in range(y, y + size):
+                for xx in range(x, x + size):
+                    List[yy][xx] = size_temp_List[yy-y][xx-x]
+
+    temp_List = copy.deepcopy(List)
+    # for i in List:
+    #     print(i)
+    # print()
+    for y in range(pow(2, N)):
+        for x in range(pow(2, N)):
+            if List[y][x] > 0:
+                cnt = 0
+                for i in range(4):
+                    yy = y + dy[i]
+                    xx = x + dx[i]
+                    if 0 <= yy < pow(2, N) and 0 <= xx < pow(2, N) and List[yy][xx] > 0:
+                        cnt += 1
+                if cnt < 3:
+                    temp_List[y][x] -= 1
+    return temp_List
+
+
+for i in cmd_List:
+    temp_List = []
+    firestorm(pow(2, i))
+    List = copy.deepcopy(firestorm(pow(2, i)))
+
+ans = 0
+Max = 0
+
+for i in List:
+    print(i)
+
+Group = [[0] * pow(2, N) for _ in range(pow(2, N))]
+for y in range(pow(2, N)):
+    for x in range(pow(2, N)):
+        ans += List[y][x]
+        if List[y][x] > 0 and Group[y][x] == 0:
+            dq = deque([[y, x]])
+            cnt = 1
+            while dq:
+                yy, xx = dq.popleft()
+                Group[yy][xx] = 1
+                for i in range(4):
+                    yyy = yy + dy[i]
+                    xxx = xx + dx[i]
+                    if 0 <= yyy < pow(2, N) and 0 <= xxx < pow(2, N) and List[yyy][xxx] > 0 and Group[yyy][xxx] == 0:
+                        dq.append([yyy, xxx])
+                        Group[yyy][xxx] = 1
+                        cnt += 1
+            Max = max(cnt, Max)
+
+# for i in List:
+#     print(i)
+
+print(ans)
+print(Max)
