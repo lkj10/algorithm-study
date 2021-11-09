cff964b478d36b19079878432b71f77baf5318c2
lkj10 <57061504+lkj10@users.noreply.github.com>
2021-11-09 18:21:21 +0900

    feat: 22주차 boj [11403]find_route.py solved! FW

--- kwangjin/22주차/[11403]find_route.py
+++ kwangjin/22주차/[11403]find_route.py
@@ -6,5 +6,13 @@ sys.stdin = open("input.txt", "r")
 N = int(input())
 List = [list(map(int, input().split())) for _ in range(N)]
 
-for i in List:
-    print(i)
+for k in range(N):
+    for y in range(N):
+        for x in range(N):
+            if List[y][k] == 1 and List[k][x] == 1:
+                List[y][x] = 1
+
+for y in range(N):
+    for x in range(N):
+        print(List[y][x], end=' ')
+    print()
